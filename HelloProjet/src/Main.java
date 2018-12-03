729e9a10ba7ed610bce50a3747f34f91453adc25
lisi <lisi@qq.com>
2018-12-03 20:19:08 +0800

    test2修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!1");
+	System.out.println("HelloWorld!11");
+	System.out.println("HelloWorld!22");
+  	System.out.println("HelloWorld!33");
+	System.out.println("HelloWorld!44");
+	System.out.println("HelloWorld!55");
+	
     }
 }
